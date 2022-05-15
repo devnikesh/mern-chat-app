008f0f952d5bb0cae641806cbe1a5bc843f04817
Nikesh <nikesh123u@gmail.com>
2022-05-15 09:27:13 +0545

    Added Debouncing

--- frontend/src/miscellaneous/SearchUser.js
+++ frontend/src/miscellaneous/SearchUser.js
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 // Chakra Imports Starts //
-import {
-  InputGroup,
-  Input,
-  Tooltip,
-  useToast,
-  Spinner,
-  InputRightElement,
-} from "@chakra-ui/react";
+import { InputGroup, Input, Tooltip, useToast, Spinner, InputRightElement } from "@chakra-ui/react";
 import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
+import { debounce } from "lodash";
 
 const SearchUser = () => {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
   // const [loadingChat, setLoadingChat] = useState(false);
   const toast = useToast();
-  const {
-    selectedChat,
-    setSelectedChat,
-    user,
-    chats,
-    setChats,
-    result,
-    setResult,
-  } = ChatState();
+  const { selectedChat, setSelectedChat, user, chats, setChats, result, setResult } = ChatState();
+
+  const deb = useCallback(
+    debounce((text) => setSearch(text), 500),
+    []
+  );
+  const updateSearch = (text) => {
+    deb(text);
+  };
+
+  useEffect(() => {
+    handleSearch(search);
+  }, [search]);
 
   const handleSearch = async (searchVal) => {
-    setSearch(searchVal);
-    console.log("working");
+    // setSearch(searchVal);
+    console.log(searchVal);
 
     try {
       setLoading(true);
@@ -38,10 +36,8 @@ const SearchUser = () => {
         },
       };
       const { data } = await axios.get(`/api/user?search=${search}`, config);
-
       setLoading(false);
       setResult(data);
-      console.log(result);
     } catch (err) {
       toast({
         title: "Error Occured ",
@@ -53,43 +49,13 @@ const SearchUser = () => {
     }
   };
 
-  const accessChat = async (userId) => {
-    try {
-      setLoading(true);
-      const config = {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${user.token}`,
-        },
-      };
-      const { data } = await axios.post("/api/chat", { userId }, config);
-      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
-      setSelectedChat(data);
-      setLoading(false);
-    } catch (err) {
-      toast({
-        title: "Error fetching the Chat",
-        description: err.message,
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  };
-
   return (
     <>
       <InputGroup size="sm">
         <Tooltip label="Search Users" placement="bottom-end">
-          <Input
-            placeholder="Search Users"
-            size="md"
-            onChange={(e) => handleSearch(e.target.value)}
-          />
+          <Input placeholder="Search Users" size="md" onChange={(e) => updateSearch(e.target.value)} />
         </Tooltip>
-        <InputRightElement
-          children={loading && <Spinner ml="auto" d="flex" />}
-        />
+        <InputRightElement children={loading && <Spinner ml="auto" d="flex" />} />
       </InputGroup>
     </>
   );
